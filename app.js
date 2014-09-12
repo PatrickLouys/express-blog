24bbc1d950dbf44168a0807d533a7ce986df0b16
Patrick <patrick@peakinformatik.com>
2014-09-12 10:33:51 +0200

    use files and dynamic routing to show content

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var markdown = require( "markdown" ).markdown;
+var fs = require('fs');
 
-app.get('/home', function(req, res){
-    res.send('Hello World');
+app.get('/:slug', function(req, res){
+    var fileName = 'pages/' + req.params.slug + '.md';
+    fs.readFile(fileName, 'utf8', function (err, data) {
+        if (err) {
+            return res.status(404).send('404 - Page not found');
+        }
+        return res.send(markdown.toHTML(data));
+    });
 });
 
 var server = app.listen(3000, function() {
