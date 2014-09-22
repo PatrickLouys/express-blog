ce745f6c8e58c15b711f64d70e38fa5bf2375b47
Patrick <patrick@peakinformatik.com>
2014-09-22 20:17:44 +0200

    added a db

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 var markdown = require( "markdown" ).markdown;
 var fs = require('fs');
+var Datastore = require('nedb');
 
-var showPage = function(req, res){
+var db = {};
+db.pages = new Datastore({ filename: 'storage/pages.db', autoload: true });
+
+
+
+var showPage = function (req, res) {
     var fileName = 'pages/' + req.params.slug + '.md';
     fs.readFile(fileName, 'utf8', function (err, data) {
         if (err) {
@@ -13,8 +19,18 @@ var showPage = function(req, res){
     });
 }
 
-app.get('/:slug', showPage);
+var addPage = function (req, res) {
+    var doc = { slug: 'hello'
+               , createdAt: new Date()
+               , content: '## This is an example markdown page stored in the db.'
+               };
 
+    db.pages.insert(doc);
+    return res.send('inserted');
+}
+
+app.get('/admin/add', addPage);
+app.get('/:slug', showPage);
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
