5e208f5c14a276ccc324607fe75a7d19d418def1
Patrick <patrick@peakinformatik.com>
2014-09-26 18:39:34 +0200

    Refactored into different files

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 var express = require('express');
 var app = express();
-var markdown = require( "markdown" ).markdown;
-var fs = require('fs');
 var Datastore = require('nedb');
+var PageHandler = require('./src/PageHandler.js');
 
-var db = {};
-db.pages = new Datastore({ filename: 'storage/pages.db', autoload: true });
-
-var getPage = function (req, res) {
-    db.pages.findOne({ "slug": req.params.slug }, function (err, doc) {
-        if (err) {
-            return res.status(500).send('500 - Error');
-        }
-
-        if (doc === null) {
-            return res.status(404).send('404 - Page not found');
-        }
-
-        return res.send(markdown.toHTML(doc.content));
-    });
-}
-
-var addPage = function (req, res) {
-    var doc = { slug: 'hello'
-               , createdAt: new Date()
-               , content: '## This is an example markdown page stored in the db.'
-               };
-
-    db.pages.insert(doc);
-    return res.send('inserted');
-}
-
-app.get('/admin/add', addPage);
-app.get('/:slug', getPage);
+app.get('/', function (req, res) { res.send('hello'); });
+app.get('/admin/add', PageHandler.addPage);
+app.get('/:slug', PageHandler.getPage);
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
---
+++ src/PageHandler.js
@@ -0,0 +1,28 @@
+var Datastore = require('nedb');
+var markdown = require( "markdown" ).markdown;
+
+var db = new Datastore({ filename: 'storage/pages.db', autoload: true });
+
+exports.getPage = function (req, res) {
+    db.findOne({ "slug": req.params.slug }, function (err, doc) {
+        if (err) {
+            return res.status(500).send('500 - Error');
+        }
+
+        if (doc === null) {
+            return res.status(404).send('404 - Page not found');
+        }
+
+        return res.send(markdown.toHTML(doc.content));
+    });
+}
+
+exports.addPage = function (req, res) {
+    var doc = { slug: 'hello'
+               , createdAt: new Date()
+               , content: '## This is an example markdown page stored in the db.'
+               };
+
+    db.pages.insert(doc);
+    return res.send('inserted');
+}
