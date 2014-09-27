e1e36cf040c7293ba93ab84a887acc9dbd0de5ca
Patrick <patrick@peakinformatik.com>
2014-09-27 16:38:49 +0200

    renamed handler to routes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var Datastore = require('nedb');
-var PageHandler = require('./src/PageHandler.js');
+var pages = require('./routes/pages.js');
 
 app.get('/', function (req, res) { res.send('hello'); });
-app.get('/admin/add', PageHandler.addPage);
-app.get('/:slug', PageHandler.getPage);
+app.get('/admin/add', pages.addPage);
+app.get('/:slug', pages.getPage);
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
